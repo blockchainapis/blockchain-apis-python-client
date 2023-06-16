f02a7e80978196a0f3bd9bd1bb126178573fdcbc
Nicolas <nicolas.went@epita.fr>
2023-06-16 13:32:53 +0200

    [BlockchainAPIs] Fixed blockchain APIs for sync

--- src/blockchainapis/BlockchainAPIsSync.py
+++ src/blockchainapis/BlockchainAPIsSync.py
@@ -27,17 +27,22 @@ from .exceptions import UnknownBlockchainAPIsException
 class BlockchainAPIsSync:
     """High-frequency DEX API
 
-        
-    Our API empowers you to access live financial data across multiple blockchains (currently supporting 6, with more on the way) with unparalleled speed and efficiency.
+    Our API empowers you to access live financial data across multiple blockchains
+    (currently supporting 6, with more on the way) with unparalleled speed and efficiency.
     
-    What sets our API apart? We've optimized performance to deliver an impressive 1000+ calls per second per user, with a lightning-fast processing time of less than 2 millisecond per request. Compare that to other solutions with a 20-millisecond processing time and fewer requests per second, and it's clear why developers choose our API for their trading bots.
+    What sets our API apart? We've optimized performance to deliver an impressive 1000+
+    calls per second per user, with a lightning-fast processing time of less than 2 millisecond
+    per request. Compare that to other solutions with a 20-millisecond processing time and fewer
+    requests per second, and it's clear why developers choose our API for their trading bots.
     
-    Another game-changing feature is our seamless integration across various blockchains and protocols. With our API, you can reuse the same code without changing a single line, simplifying the development process and saving you valuable time.
+    Another game-changing feature is our seamless integration across various blockchains and
+    protocols. With our API, you can reuse the same code without changing a single line, simplifying
+    the development process and saving you valuable time.
     
-    Ready to try it out? [Sign up for a free API key here](https://dashboard.blockchainapis.io) or start exploring the possibilities on this page. Need support or have questions? Join our [Discord community](https://discord.gg/GphRMJXmS5) where our team and fellow developers are eager to help you make the most of our powerful API.
-
-    Please note that this class is not async which is less optimized. To run more otpimized
-    requests, please use: BlockchainAPIs
+    Ready to try it out? [Sign up for a free API key here](https://dashboard.blockchainapis.io)
+    or start exploring the possibilities on this page. Need support or have questions? Join our
+    [Discord community](https://discord.gg/GphRMJXmS5) where our team and fellow developers are
+    eager to help you make the most of our powerful API.
     """
     
     def __init__(self, api_key: str | None = None):
